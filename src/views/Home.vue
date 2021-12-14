afc4d2b04b61430259bb5f9b2a048292ae9545b6
David Adewole <adeoluwadavid@gmail.com>
2021-12-14 08:44:42 +0100

    vue-task-tracker

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-show="showAddTask"><AddTask @add-task="addTask" /></div>
+
+  <Tasks
+    @toggle-reminder="toggleReminder"
+    @delete-task="deleteTask"
+    :tasks="tasks"
+  />
+</template>
+<script>
+// import Tasks from "./components/Tasks";
+// import AddTask from "./components/AddTask";
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
